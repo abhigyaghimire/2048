2740c3ad05d74de6ed73398a568924a4832d08c1
abhigyaghimire <abhigyaghimire@users.noreply.github.com>
2016-03-23 12:42:45 -0700

    Update Start.java

--- Start.java
+++ Start.java
@@ -1,9 +1,6 @@
 import java.util.InputMismatchException;
 /**
  * @author: Abhigya Ghimire
- * @email:  aghimire@ucsd.edu
- * @PID:    A11628841
- * @user:   cs8bbe
  * @about:   Start class has main method which creates a new Board, makes a Game 
  * 			 using the created board, and then calls play() method for the program 
  *   		 to execute. 
