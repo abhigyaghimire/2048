edb1b5a91b0413e065675672fa305bb362a652c3
abhigyaghimire <abhigyaghimire@users.noreply.github.com>
2016-03-23 12:42:00 -0700

    Update Direction.java

--- Direction.java
+++ Direction.java
@@ -1,8 +1,5 @@
 /**
  * @author: Abhigya Ghimire
- * @email:  aghimire@ucsd.edu
- * @PID:    A11628841
- * @user:   cs8bbe
  * @about:   Direction is an enumerator type which consists of the commands 
  * 			 that users can give to the program. 
  */
