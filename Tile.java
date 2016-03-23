ba9a09cd7fff617dc3da98f5f095d817b0afec2c
abhigyaghimire <abhigyaghimire@users.noreply.github.com>
2016-03-23 12:43:14 -0700

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -1,8 +1,5 @@
 /**
  * @author: Abhigya Ghimire
- * @email:  aghimire@ucsd.edu
- * @PID:    A11628841
- * @user:   cs8bbe
  * @about:   Tile creates the tiles of the game where the values (2 ^n) are stored.
  *		 	 Tile class makes it easy for users to create a board in Board class using a 
  *  		 2d Tile array. 
